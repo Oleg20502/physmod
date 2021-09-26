a59b26fc763be756d4ae5f4b12f8c76c97a55eaa
Oleg20502 <oleg20502@gmail.com>
2021-09-26 11:23:42 +0300

    26.09.2021 update

--- wk_02/6.py
+++ wk_02/6.py
@@ -14,7 +14,7 @@ a = np.arange(3*4*5*6).reshape((3,4,5,6))
 
 b = np.arange(3*4*5*6)[::-1].reshape((5,4,6,3))
 
-print(np.dot(a, b)[2,3,2,1,2,2]
+print(np.dot(a, b)[2,3,2,1,2,2])
 
 #%%
 w, v = np.linalg.eig(np.diag((1, 2, 3)))
